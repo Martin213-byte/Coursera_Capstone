d426829fa90de77a70604dc796b3cc9421fadb7f
Martin Koshnicharov <martin_koshnicharov@abv.bg>
2021-01-12 19:38:04 +0100

    secure the files

---
+++ dataset.js
@@ -0,0 +1,350 @@
+var dataset = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Bankya",
+        "Latitude": 42.7101251,
+        "Longitude": 23.1464966
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.1464966,
+          42.7101251
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Vitosha",
+        "Latitude": 42.56,
+        "Longitude": 23.28
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.28,
+          42.56
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Vrabnitsa",
+        "Latitude": 42.759103350000004,
+        "Longitude": 23.247336814386458
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.247336814386458,
+          42.759103350000004
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Vazrazhdane",
+        "Latitude": 43.2137765,
+        "Longitude": 27.9111022
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          27.9111022,
+          43.2137765
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Izgrev",
+        "Latitude": 42.67048135,
+        "Longitude": 23.351794403651937
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.351794403651937,
+          42.67048135
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Ilinden",
+        "Latitude": 41.993768,
+        "Longitude": 21.5775059
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          21.5775059,
+          41.993768
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Iskar",
+        "Latitude": 43.4597596,
+        "Longitude": 24.2557605
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.2557605,
+          43.4597596
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Krasna polyana",
+        "Latitude": 42.6943112,
+        "Longitude": 23.2798074
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.2798074,
+          42.6943112
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Krasno selo",
+        "Latitude": 42.6736589,
+        "Longitude": 23.2825005
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.2825005,
+          42.6736589
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Kremikovtsi",
+        "Latitude": 42.7843029,
+        "Longitude": 23.499576
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.499576,
+          42.7843029
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Lozenets",
+        "Latitude": 42.20761,
+        "Longitude": 27.80723
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          27.80723,
+          42.20761
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Lyulin",
+        "Latitude": 42.7179387,
+        "Longitude": 23.257979
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.257979,
+          42.7179387
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Mladost",
+        "Latitude": 42.6475353,
+        "Longitude": 23.382125068194245
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.382125068194245,
+          42.6475353
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Nadezhda",
+        "Latitude": 49.740276,
+        "Longitude": 34.211388
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          34.211388,
+          49.740276
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Novi Iskar",
+        "Latitude": 42.8143844,
+        "Longitude": 23.3582609
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.3582609,
+          42.8143844
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Ovcha kupel",
+        "Latitude": 42.6778469,
+        "Longitude": 23.2622161
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.2622161,
+          42.6778469
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Oborishte",
+        "Latitude": 42.6958089,
+        "Longitude": 23.343018531444415
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.343018531444415,
+          42.6958089
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Pancharevo",
+        "Latitude": 42.5944337,
+        "Longitude": 23.4054539
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.4054539,
+          42.5944337
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Poduene",
+        "Latitude": 42.707064599999995,
+        "Longitude": 23.368427952748874
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.368427952748874,
+          42.707064599999995
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Serdika",
+        "Latitude": 42.7337581,
+        "Longitude": 23.337294951063065
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.337294951063065,
+          42.7337581
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Slatina",
+        "Latitude": 44.4299971,
+        "Longitude": 24.3631837
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          24.3631837,
+          44.4299971
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Studentski",
+        "Latitude": 42.6466473,
+        "Longitude": 23.349826275873603
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          23.349826275873603,
+          42.6466473
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Sredets",
+        "Latitude": 42.3458456,
+        "Longitude": 27.1808724
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          27.1808724,
+          42.3458456
+        ]
+      }
+    }
+  ]
+}
